d158969133ae7db4a02b93524191112f0288c40c
sungbolee <sungbobackend@gmail.com>
2022-10-14 12:04:20 +0900

    feat: Spring Data JPA Dependency 추가

--- src/main/java/com/example/atddstudy/application/CreateUserServiceImpl.java
+++ src/main/java/com/example/atddstudy/application/CreateUserServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-class CreateUserServiceImpl implements CreateUserService {
+public class CreateUserServiceImpl implements CreateUserService {
 
     private final UserRepository userRepository;
 
